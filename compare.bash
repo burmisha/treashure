f7c7f64e9189f8cc5e6ed18bd6a81d7637e67d76
Mikhail Burmistrov <burmisha@gmail.com>
2020-09-15 17:42:48 +0300

    Better copy of compare in bash

--- compare.bash
+++ compare.bash
@@ -56,3 +56,47 @@ compare_dirs () {
 # compare_dirs '/d/PhotoPhotoshop' '/H/PhotoPhotoshop'
 compare_dirs '/d/Photo' '/H/Photo'
 # compare_dirs '/d/tmp/a' '/d/tmp/b'
+
+
+list_files () {
+  local dirname="${1}"
+  local dstfile="${2}"
+  cd ${dirname}
+  find . -type f | wc -l | awk '{print "Found "$1" files in '${dirname}'"}'
+  # find . -type f > "${dstfile}"
+  find . -type f -exec md5 '{}' \; > ${dstfile}
+  sort "${dstfile}" -o "${dstfile}"
+  sed -e 's|MD5 (.*) = ||g' "${dstfile}" ${dstfile} | sort | uniq -c | sort -k1,1rn > ${dstfile}.md5
+  cd -
+}
+
+compare () {
+  local macbookDir="${1}"
+  local backupDir="${2}"
+
+  list_files ${macbookDir} ~/macbook.txt
+  list_files ${backupDir} ~/backup.txt
+
+  diff ~/macbook.txt ~/backup.txt > ~/diff.txt 
+  head -20 ~/diff.txt 
+  wc ~/macbook.txt ~/backup.txt
+  grep '^>' ~/diff.txt | wc -l | awk '{print "Backup has "$1" more files"}'
+  grep '^<' ~/diff.txt | wc -l | awk '{print "Backup is missing "$1" files"}'
+  diff ~/macbook.txt.md5 ~/backup.txt.md5
+}
+
+# cp -r /Users/burmisha/Documents/Zoom /Volumes/tosh2tb/MacBackup_2020-09/Zoom
+compare /Users/burmisha/Documents/Zoom /Volumes/tosh2tb/MacBackup_2020-09/Zoom
+# rm -r /Users/burmisha/Documents/Zoom.broken.path
+
+compare /Users/burmisha/PhotoLocal /Volumes/tosh2tb/MacBackup_2020-09/PhotoLocal
+
+compare /Users/burmisha/Downloads /Volumes/tosh2tb/MacBackup_2020-09/Downloads
+compare /Users/burmisha/tmp /Volumes/tosh2tb/MacBackup_2020-09/tmp
+compare /Users/burmisha/video /Volumes/tosh2tb/MacBackup_2020-09/video
+
+grep 60749ec6a25f49ca5488bbd5fc8d445c ~/macbook.txt ~/backup.txt
+grep 359fd342409d4be515a7cb534d24f375 ~/macbook.txt ~/backup.txt
+grep 1f4b21f0677476530632291503b9a447 ~/macbook.txt ~/backup.txt
+grep 78b900a2cf616cfb79b477d4ab961b5f ~/macbook.txt ~/backup.txt
+grep a69f20a9547439f8778f2c77789e3d55 ~/macbook.txt ~/backup.txt
