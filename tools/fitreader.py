3ac37bd3a50f2160a2143eee4373786f1cef588e
Mikhail Burmistrov <burmisha@gmail.com>
2022-10-16 00:36:46 +0400

    Clean up

--- tools/fitreader.py
+++ tools/fitreader.py
@@ -1,22 +1,33 @@
 import fitparse
 import datetime
 
-import pprint
 import os
 
-from typing import Tuple, List, Optional
+from typing import Tuple, List
 from tools.model import GeoPoint, Track
 
 import logging
 log = logging.getLogger(__name__)
 
 
-def __from_semicircles(value):
+RECORD_MESSAGE = 'record'
+ACTIVITY_MESSAGE = 'activity'
+
+
+class InvalidTimezone(Exception):
+    pass
+
+
+class InvalidPoint(Exception):
+    pass
+
+
+def __from_semicircles(value: float) -> float:
     # https://forums.garmin.com/forum/developers/garmin-developer-information/60220-
     return float(value) * 180 / (2 ** 31)
 
 
-def parse_values(values: dict) -> Optional[GeoPoint]:
+def get_point(values: dict) -> GeoPoint:
     timestamp = int((values['timestamp'] - datetime.datetime(1970, 1, 1)).total_seconds())
     assert 1000000000 < timestamp < 2000000000
     if 'enhanced_altitude' in values:
@@ -25,14 +36,14 @@ def parse_values(values: dict) -> Optional[GeoPoint]:
     longitude = values.get('position_long')
     latitude = values.get('position_lat')
     if (longitude is None) or (latitude is None):
-        return None
+        raise InvalidPoint('No longitude on latitude')
 
     altitude = values['altitude']
     assert altitude is not None
 
     longitude = __from_semicircles(longitude)
     latitude = __from_semicircles(latitude)
-    point = GeoPoint(
+    return GeoPoint(
         longitude=float(f'{longitude:.9f}'),
         latitude=float(f'{latitude:.9f}'),
         altitude=float(f'{altitude:.3f}'),
@@ -40,15 +51,10 @@ def parse_values(values: dict) -> Optional[GeoPoint]:
         heart_rate=values.get('heart_rate') or None,
         timestamp=timestamp,
     )
-    return point
 
 
-def get_points(filename, check_crc: bool) -> Track:
-    points = []
-    failures = []
-
-    fit_file = fitparse.FitFile(filename, check_crc=check_crc)
-    activity_messages = list(fit_file.get_messages('activity'))
+def get_activity_timezone(fit_file: fitparse.FitFile) -> datetime.timezone:
+    activity_messages = list(fit_file.get_messages(ACTIVITY_MESSAGE))
     assert len(activity_messages) == 1
     activity_values = activity_messages[0].get_values()
     timestamp = activity_values['timestamp']
@@ -59,38 +65,45 @@ def get_points(filename, check_crc: bool) -> Track:
         datetime.timedelta(seconds=10800),
         datetime.timedelta(seconds=14400),
     ]:
-        raise RuntimeError(f'Invalid timezone for {filename}: {timedelta}')
-    activity_timezone = datetime.timezone(timedelta)
+        raise InvalidTimezone(f'Invalid timezone: {timedelta}')
 
-    for message_index, message in enumerate(fit_file.get_messages(name='record')):
-        values = message.get_values()
+    return datetime.timezone(timedelta)
+
+
+def get_points_and_failures(fit_file: fitparse.FitFile) -> Tuple[List[GeoPoint], List[int]]:
+    points = []
+    failures = []
+    for message_index, message in enumerate(fit_file.get_messages(name=RECORD_MESSAGE)):
         try:
-            point = parse_values(values)
-            if point:
-                points.append(point)
-            else:
-                failures.append(message_index)
+            values = message.get_values()
+            points.append(get_point(values))
+        except InvalidPoint:
+            failures.append(message_index)
         except:
             log.error(f'failed at {message_index}')
             raise
 
-    return Track(
-        filename=filename,
-        points=points,
-        failures=failures,
-        activity_timezone=activity_timezone
-    )
+    return points, failures
 
 
 def read_fit_file(filename, raise_on_error=True) -> Track:
-    log.debug(f'Loading {filename}')
     try:
-        track = get_points(filename, check_crc=True)
-        track.correct_crc = True
+        fit_file = fitparse.FitFile(filename, check_crc=True)
+        crc_ok = True
     except fitparse.utils.FitCRCError:
-        log.debug(f'FitCRCError on {filename}')
-        track = get_points(filename, check_crc=False)
-        track.correct_crc = False
+        fit_file = fitparse.FitFile(filename, check_crc=False)
+        crc_ok = False
+
+    activity_timezone = get_activity_timezone(fit_file)
+    points, failures = get_points_and_failures(fit_file)
+
+    track = Track(
+        filename=filename,
+        points=points,
+        failures=failures,
+        activity_timezone=activity_timezone,
+        correct_crc=crc_ok,
+    )
 
     if raise_on_error and not track.is_valid:
         log.error(f'{track!r}')
