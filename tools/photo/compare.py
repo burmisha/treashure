4f114be888568ebcd0ba243dac813fad3b79f55a
Mikhail Burmistrov <burmisha@gmail.com>
2022-11-09 23:26:23 +0400

    Run photo-calc

--- tools/photo/compare.py
+++ tools/photo/compare.py
@@ -1,132 +1,174 @@
-import argparse
-import hashlib
 import json
 import os
 import platform
 
 from collections import defaultdict
+from enum import Enum
+from typing import Dict, List, Tuple
+
+import attr
 
 import logging
 log = logging.getLogger(__file__)
 
+from library.md5sum import Md5Sum
+from library.files import walk
+
+
+class Platform(str, Enum):
+    Windows = 'win'
+    macOS = 'osx'
+
+
+class Mode(str, Enum):
+    Old = 'old'
+    New = 'new'
+
+
+def get_platform() -> Platform:
+    system = platform.system()
+    if system == 'Windows':
+        return Platform.Windows
+    elif system == 'Darwin':
+        return Platform.macOS
+    else:
+        raise RuntimeError(f'Invalid platform system {system!r}')
+    log.info(f'Running in {mode!r} mode')
+
+
+ya_disk_dir = {
+    Platform.Windows: os.path.join('D:' + os.sep, 'YandexDisk'),
+    Platform.macOS: os.path.join(os.environ['HOME'], 'Yandex.Disk.localized'),
+}[get_platform()]
+
 
 class Defaults(object):
     def __init__(self):
-        system = platform.system()
-        if system == 'Windows':
-            mode = 'win'
-        elif system == 'Darwin':
-            mode = 'osx'
-        else:
-            raise RuntimeError('Invalid platform system {!r}'.format(system))
-        log.info('Running in {!r} mode'.format(mode))
-        self.System = mode
-        self.OldJsonName = 'old_hashes_4.json'
-        self.NewJsonName = 'new_hashes_4.json'
+        self.OldJsonName = 'old_hashes_5.json'
+        self.NewJsonName = 'new_hashes_5.json'
 
-    def GetJsonLocation(self, oldOrNew):
-        basename = self.OldJsonName if oldOrNew == 'old' else self.NewJsonName
-        yaDisk = {
-            'win': os.path.join('D:' + os.sep, 'YandexDisk'),
-            'osx': os.path.join(os.sep, 'Users', 'burmisha', 'Yandex.Disk.localized'),
-        }
-        return os.path.join(yaDisk[self.System], 'tmp', basename)
+    def GetJsonLocation(self, mode: Mode) -> str:
+        basename = {
+            mode.Old: self.OldJsonName,
+            mode.New: self.NewJsonName,
+        }[mode]
 
-    def GetRootLocation(self, oldOrNew):
+        return os.path.join(ya_disk_dir, basename)
+
+    def GetRootLocation(self, mode: Mode):
         paths = {
-        #     'old': os.path.join(os.sep, 'Volumes', '1Tb_2014', '2015-05-30_photo'),
-        #     'new': os.path.join('D:' + os.sep, 'Photo'),
-        #     'old': os.path.join('D:' + os.sep, 'PhotoPhotoshop'),
-        #     'new': os.path.join('K:' + os.sep, 'PhotoPhotoshop'),
-        #     'old': os.path.join(os.sep, 'Volumes', 'bur300gb'),
-        #     'new': os.path.join('K:' + os.sep, 'bur300gb'),
-            'old': os.path.join(os.sep, 'Users', 'burmisha', 'ene2nd'),
-            'new': os.path.join('K:' + os.sep, 'ene2nd'),
+            # mode.Old: os.path.join(os.sep, 'Volumes', '1Tb_2014', '2015-05-30_photo'),
+            # mode.New: os.path.join('D:' + os.sep, 'Photo'),
+            # mode.Old: os.path.join('D:' + os.sep, 'PhotoPhotoshop'),
+            # mode.New: os.path.join('K:' + os.sep, 'PhotoPhotoshop'),
+            # mode.Old: os.path.join(os.sep, 'Volumes', 'bur300gb'),
+            # mode.New: os.path.join('K:' + os.sep, 'bur300gb'),
+            # mode.Old: os.path.join(os.environ['HOME'], 'ene2nd'),
+            # mode.New: os.path.join('K:' + os.sep, 'ene2nd'),
+            mode.Old: os.path.join(ya_disk_dir, 'Photo', '2020'),
         }
-        return paths[oldOrNew]
+        return paths[mode]
 
 
 defaults = Defaults()
 
 
-def Md5Sum(filename):
-    hash_md5 = hashlib.md5()
-    with open(filename, 'rb') as f:
-        for chunk in iter(lambda: f.read(2 ** 16), b''):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
 def runCalc(args):
     rootDir, hashesFile = defaults.GetRootLocation(args.mode), defaults.GetJsonLocation(args.mode)
-    rootDir = unicode(rootDir.decode('utf-8'))
     if not os.path.isdir(rootDir):
-        raise RuntimeError('{} is not a directory'.format(logDir(rootDir)))
+        raise RuntimeError(f'{rootDir} is not a directory')
     else:
-        log.info(u'Detected root at {}'.format(logDir(rootDir)))
+        log.info(f'Detected root at {rootDir}')
 
-    log.info('Calculating stats in {} and writing to {}'.format(logDir(rootDir), logDir(hashesFile)))
+    log.info(f'Calculating stats in {rootDir} and writing to {hashesFile}')
 
-    hashes = Hashes(hashesFile)
-    hashes.Root = rootDir
+    hashes = Hashes(
+        filename=hashesFile,
+        root=rootDir,
+    )
 
     for root, _, files in os.walk(rootDir):
-        files = list(files)
-        if files:
-            log.info('Found {} files in {}'.format(len(files), logDir(root)))
-            localName = root.replace(rootDir + os.sep, '').replace(os.sep, '/')
-            hashes.Tree[localName] = dict((file, Md5Sum(os.path.join(root, file))) for file in files)
+        hash_by_name = {file: Md5Sum(os.path.join(root, file)) for file in files}
+        if not hash_by_name:
+            continue
+        localized_root = root.replace(rootDir + os.sep, '').replace(os.sep, '/')
+        log.info(f'Found {len(hash_by_name):3d} files in {localized_root!r}')
+        hashes.tree[localized_root] = hash_by_name
 
     hashes.Save()
 
-def logDir(dirName):
-    return '\'{}\''.format(dirName.encode('utf-8'))
 
-class Hashes(object):
-    def __init__(self, filename):
-        self.Filename = filename
-        self.Root = None
-        self.Tree = {}
+@attr.s
+class Hashes:
+    filename: str = attr.ib()
+    root: str = attr.ib()
+    tree: Dict[str, Dict[str, str]] = attr.ib(default={})
 
-    def Load(self):
-        with open(self.Filename) as f:
-            hashes = json.loads(f.read())
-            self.Root = hashes['root']
-            self.Tree = hashes['tree']
-            log.info('Found root {}'.format(logDir(self.Root)))
+    @classmethod
+    def load(self, filename):
+        with open(filename) as f:
+            data = json.load(f)
+
+        return Hashes(
+            filename=filename,
+            root=data['root'],
+            tree=data['tree'],
+        )
 
     def Save(self):
-        log.info('Saving info about {} to {}'.format(logDir(self.Root), logDir(self.Filename)))
-        with open(self.Filename, 'w') as f:
+        log.info(f'Saving info about {self.root} to {self.filename}')
+        with open(self.filename, 'w') as f:
             json.dump(
                 {
-                    'root': self.Root,
-                    'tree': self.Tree,
+                    'root': self.root,
+                    'tree': self.tree,
                 },
                 f,
                 sort_keys=True,
                 indent=4,
-                separators=(',', ': ')
+                separators=(',', ': '),
+                ensure_ascii=False,
             )
 
-
-class Comparator(object):
-    def __init__(self, oldHashes, newHashes):
-        self.OldHashes = Hashes(oldHashes)
-        self.NewHashes = Hashes(newHashes)
-
-        self.OldHashes.Load()
-        self.NewHashes.Load()
-
-    def Index(self):
+    @property
+    def files_by_hash_index(self):
         log.info('Building new files index')
-        oldFilesHashes = defaultdict(list)
-        for newDir, newHashes in self.NewHashes.Tree.iteritems():
-            for fileName, fileHash in newHashes.iteritems():
-                oldFilesHashes[fileHash].append(newDir)
-                if len(oldFilesHashes[fileHash]) >= 4:
-                    log.info('File {!r} with hash {} is among {} dirs'.format(fileName, fileHash, len(oldFilesHashes[fileHash])))
+        files_by_hash = defaultdict(list)
+        for new_dir, new_hashes in self.tree.items():
+            for new_file, new_hash in new_hashes.items():
+                files_by_hash[new_hash].append(new_dir)
+                if len(files_by_hash[new_hash]) >= 4:
+                    log.info(f'File {new_file!r} with hash {new_hash} is among {len(files_by_hash[new_hash])} dirs')
 
-        return oldFilesHashes
+        return index
+
+    @property
+    def clean_tree_items(self) -> List[Tuple[str, List[str]]]:
+        skipDirs = [' Previews.lrdata', 'Lightroom Settings']
+        skipFiles = ['.DS_Store']
+
+        tree_items = []
+        for oldDir, oldHashes in self.tree.items():
+            if any(skipDir in oldDir for skipDir in skipDirs):
+                continue
+            hashes = [
+                (filename, fileHash)
+                for filename, fileHash in oldHashes.iteritems()
+                if filename in skipFiles
+            ]
+            if not hashes:
+                continue
+            hashes.sort()
+            tree_items.append(oldDir, hashes)
+
+        tree_items.sort()
+        return tree_items
+
+
+class Comparator:
+    def __init__(self, old_hashes_file: str, new_hashes_file: str):
+        self.OldHashes = Hashes.load(old_hashes_file)
+        self.NewHashes = Hashes.load(new_hashes_file)
 
     def VisualizeStates(self, exists):
         if any(exists):
@@ -143,30 +185,13 @@ class Comparator(object):
         else:
             return 'All files are missing\n'
 
-    def OldItems(self):
-        skipDirs = [' Previews.lrdata', 'Lightroom Settings']
-        skipFiles = ['.DS_Store']
-        treeItems = self.OldHashes.Tree.items()
-        treeItems.sort()
-        for oldDir, oldHashes in treeItems:
-            if any(skipDir in oldDir for skipDir in skipDirs):
-                continue
-            else:
-                hashes = []
-                for fileName, fileHash in oldHashes.iteritems():
-                    if fileName not in skipFiles:
-                        hashes.append((fileName, fileHash))
-                if hashes:
-                    hashes.sort()
-                    yield oldDir, hashes
-
     def __call__(self):
-        oldFilesHashes = self.Index()
-        for oldDir, oldHashes in self.OldItems():
+        new_files_hashes = self.NewHashes.files_by_hash_index
+        for oldDir, oldHashes in self.OldHashes.clean_tree_items:
             dirs = defaultdict(int)
             exists = []
             for fileName, fileHash in oldHashes:
-                fileDirs = oldFilesHashes.get(fileHash, [])
+                fileDirs = new_files_hashes.get(fileHash, [])
                 exists.append(bool(fileDirs))
                 for d in fileDirs:
                     dirs[d] += 1
@@ -211,14 +236,14 @@ class Comparator(object):
 
 def runPrint(args):
     comparator = Comparator(
-        defaults.GetJsonLocation('old'),
-        defaults.GetJsonLocation('new'),
+        old_hashes_file=defaults.GetJsonLocation(Mode.Old),
+        new_hashes_file=defaults.GetJsonLocation(Mode.New),
     )
     comparator()
 
 
 def populate_calc_parser(parser):
-    parser.add_argument('--mode', help='Choose mode', choices=['old', 'new'], required=True)
+    parser.add_argument('--mode', help='Choose mode', choices=[mode.value for mode in Mode], required=True, type=Mode)
     parser.set_defaults(func=runCalc)
 
 
