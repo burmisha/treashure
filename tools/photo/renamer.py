cb85c0373e0b5a5738fbeeec77e8484decfd461c
Mikhail Burmistrov <burmisha@gmail.com>
2023-01-10 22:19:33 +0400

    Clena up in rename tool

--- tools/photo/renamer.py
+++ tools/photo/renamer.py
@@ -4,6 +4,8 @@ import logging
 import collections
 import os
 
+from typing import List, Dict
+
 log = logging.getLogger(__name__)
 
 NAME_FORMAT = '{dt:%Y-%m-%d %H-%M-%S}{suffix}.{extension}'
@@ -11,33 +13,38 @@ NAME_FORMAT = '{dt:%Y-%m-%d %H-%M-%S}{suffix}.{extension}'
 
 class FileMover:
     def __init__(self):
-        self.move_list = []
-        self.src_set = set()
-        self.dst_set = set()
+        self._move_list = []
+        self._src_set = set()
+        self._dst_set = set()
 
     def add(self, src, dst):
         if src == dst:
             log.debug(f'Same location, skip: {src!r}')
             return
 
-        if src in self.src_set:
-            raise RuntimeError(f'Duplicated move from: {src!r}')
-        if dst in self.dst_set:
-            raise RuntimeError(f'Duplicated move to: {dst!r}')
+        if src in self._src_set:
+            raise RuntimeError(f'Duplicated move src: {src!r}')
+        if dst in self._dst_set:
+            raise RuntimeError(f'Duplicated move dst: {dst!r}')
         if os.path.exists(dst):
             raise RuntimeError(f'Dst already exists: {dst!r}')
 
-        self.move_list.append((src, dst))
-        self.src_set.add(src)
-        self.dst_set.add(dst)
+        self._move_list.append((src, dst))
+        self._src_set.add(src)
+        self._dst_set.add(dst)
 
-    def move(self):
-        for src, dst in self.move_list:
+    def get_src_dst(self):
+        log.info(f'Got {len(self._move_list)} files to move')
+        for src, dst in self._move_list:
             log.info(f'mv {src!r} {dst!r}')
-            os.rename(src, dst)
+            yield src, dst
 
-    def __len__(self):
-        return len(self.move_list)
+    def get_src_dirnames(self) -> Dict[str, List[str]]:
+        dirnames = collections.defaultdict(list)
+        for src, _ in self._move_list:
+            dirname = os.path.dirname(src)
+            dirnames[dirname].append(src)
+        return dirnames
 
 
 def file_is_ok(filename: str) -> bool:
@@ -50,7 +57,6 @@ def rename_dir(
     *,
     dir_name: str,
     move: bool=False,
-    add_log: bool=False,
 ):
     photo_files = [
         tools.mobile.mobile.PhotoFile(file)
@@ -62,11 +68,9 @@ def rename_dir(
     log.warn(f'Checking {len(photo_files)} photo files in {dir_name}')
 
     photos_by_ts = collections.defaultdict(list)
-    ok_count = 0
     for photo in photo_files:
         if photo.timestamp:
             photos_by_ts[photo.timestamp].append(photo)
-            ok_count += 1
         else:
             log.warn(f'No ts in file, skip: {photo.Path}')
 
@@ -84,25 +88,22 @@ def rename_dir(
             dst_file = os.path.join(os.path.dirname(photo.Path), dst_basename)
             mover.add(photo.Path, dst_file)
 
-    log.info(f'Got {len(mover)} files to move')
+    for src, dst in mover.get_src_dst():
+        if move:
+            os.rename(src, dst)
 
-    if move:
-        mover.move()
-    elif add_log:
-        for src, dst in mover.move_list:
-            log.info(f'will move {src!r} {dst!r}')
+    for dirname, files in mover.get_src_dirnames().items():
+        log.info(f'Dir {dirname} has {len(files)} files: {os.path.basename(files[0])} .. {os.path.basename(files[-1])}')
 
 
 def run_rename(args):
     rename_dir(
         dir_name=args.dir,
         move=args.move,
-        add_log=args.add_log,
     )
 
 
 def populate_parser(parser):
     parser.add_argument('--dir', help='Add dir to rename', required=True)
     parser.add_argument('--move', help='Do move', action='store_true')
-    parser.add_argument('--add-log', help='log', action='store_true')
     parser.set_defaults(func=run_rename)
