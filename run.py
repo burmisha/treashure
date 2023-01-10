08430fe428fb4b8fc0b2535bf81abaf60ddf3f9b
Mikhail Burmistrov <burmisha@gmail.com>
2023-01-10 22:19:03 +0400

    Group subcommands

--- run.py
+++ run.py
@@ -9,26 +9,28 @@ import logging
 log = logging.getLogger('treashure')
 
 
+COMMANDS = [
+    ('import', 'Import tracks from device', tools.import_tracks.populate_parser),
+    ('join', 'Join old tracks into one', tools.join_tracks.populate_parser),
+    ('analyze', 'Analyze files', tools.speed.populate_parser),
+    ('photo-parse', 'Parse mobile photos', tools.mobile.analyze.populate_parser),
+    ('photo-analyze', 'Analyze mobile photos', tools.mobile.mobile.populate_parser),
+    ('photo-calc', 'Run calc', tools.photo.compare.populate_calc_parser),
+    ('photo-compare', 'Run compare', tools.photo.compare.populate_compare_parser),
+    ('flickr-parse', 'Prepare photos', tools.photo.parse.populate_parser),
+    ('photo-rename', 'Rename vsco photos', tools.photo.renamer.populate_parser),
+]
+
+
 def create_arguments_parser():
     formatter_class = argparse.ArgumentDefaultsHelpFormatter
     parser = argparse.ArgumentParser('Common runner', formatter_class=formatter_class)
     parser.add_argument('--debug', help='Debug logging', action='store_true')
 
     subparsers = parser.add_subparsers()
-
-    def add_subparser(cmd: str, desc: str, populate_func):
+    for cmd, desc, populate_func in COMMANDS:
         subparser = subparsers.add_parser(cmd, help=desc, formatter_class=formatter_class)
         populate_func(subparser)
-
-    add_subparser('import', 'Import tracks from device', tools.import_tracks.populate_parser)
-    add_subparser('join', 'Join old tracks into one', tools.join_tracks.populate_parser)
-    add_subparser('analyze', 'Analyze files', tools.speed.populate_parser)
-    add_subparser('photo-parse', 'Parse mobile photos', tools.mobile.analyze.populate_parser)
-    add_subparser('photo-analyze', 'Analyze mobile photos', tools.mobile.mobile.populate_parser)
-    add_subparser('photo-calc', 'Run calc', tools.photo.compare.populate_calc_parser)
-    add_subparser('photo-compare', 'Run compare', tools.photo.compare.populate_compare_parser)
-    add_subparser('flickr-parse', 'Prepare photos', tools.photo.parse.populate_parser)
-    add_subparser('photo-rename', 'Rename vsco photos', tools.photo.renamer.populate_parser)
 
     return parser
 
